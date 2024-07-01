644b80abf01951419c76f1dae23c6bc1ba273f44
tibitTW <chunhua.2147483647@gmail.com>
2024-07-01 17:54:42 +0800

    refactor: revise constant path
    rewrite paths' generation methods

--- src/base/constants/path.py
+++ src/base/constants/path.py
@@ -1,5 +1,8 @@
 import os
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(BASE_DIR, "../../data/database.db")
-SQL_SRC_PATH = os.path.join(BASE_DIR, "../../specs/db/struct.sql")
+# project root dir
+__current_path = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.normpath(os.path.join(__current_path, "../../../"))
+DB_PATH = os.path.join(BASE_DIR, "data/database.db")
+SQL_SRC_PATH = os.path.join(BASE_DIR, "specs/sql/struct.sql")
+CSV_INJECTION_DIR_PATH = os.path.join(BASE_DIR, "specs/csv")
