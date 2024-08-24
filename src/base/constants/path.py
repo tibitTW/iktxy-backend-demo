b077f07e1efd5ac00f7d9decacad7e7d5d8495b5
tibitTW <chunhua.2147483647@gmail.com>
2024-08-25 00:07:46 +0800

    refactor: remove old constants file
    - remove src/base/constants/path.py

--- src/base/constants/path.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# project root dir
-__current_path = os.path.dirname(os.path.abspath(__file__))
-BASE_DIR = os.path.normpath(os.path.join(__current_path, "../../../"))
-DB_PATH = os.path.join(BASE_DIR, "data/database.db")
-SQL_SRC_PATH = os.path.join(BASE_DIR, "specs/sql/struct.sql")
-CSV_INJECTION_DIR_PATH = os.path.join(BASE_DIR, "specs/csv")
