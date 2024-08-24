69735c03a96c248ac6764437c157660d5a5a00d6
tibitTW <chunhua.2147483647@gmail.com>
2024-08-25 00:05:38 +0800

    fix: revise db init script
    - rename from db_creation.py -> db_init.py
    - revise the way of import constants

--- scripts/init/db_init.py
+++ scripts/init/db_init.py
@@ -1,27 +1,22 @@
-from src.base.constants.path import (
-    CSV_INJECTION_DIR_PATH,
-    DB_PATH,
-    SQL_SRC_PATH,
-)
+from src.base.constants import DatabasePaths as DBPath
 from src.dbapi.sqlite3.main import SQLiteDB
 
 
 def main():
-    print(DB_PATH)
-    db = SQLiteDB(db_path=DB_PATH)
+    db = SQLiteDB(db_path=DBPath.DB_PATH)
 
     # clear old database
     db.clear()
     db.connect()
 
     # create database structure with existing .sql file
-    db.load_sql(sql_filepath=SQL_SRC_PATH)
+    db.load_sql(sql_filepath=DBPath.SQL_SRC_PATH)
 
     # insert sample data
     tables = ["users", "programs", "holly_houses", "participate_records"]
     for table in tables:
         db.insert_csv_data(
-            csv_path=f"{CSV_INJECTION_DIR_PATH}/{table}.csv",
+            csv_path=f"{DBPath.CSV_INJECTION_DIR_PATH}/{table}.csv",
             table_name=table,
         )
 
